0ad4c78056a9f6ae93657571c301fc10b1be7906
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-03-25 19:01:58 -0500

    first commit

---
+++ assets/javascript/moment.js
@@ -0,0 +1,19 @@
+$(document).ready(function{
+
+	var config = {
+	    apiKey: "AIzaSyAozjWY5VG4mrX1xUGE9kMb88rtBgO6nCI",
+	    authDomain: "train-schedule-8c9c6.firebaseapp.com",
+	    databaseURL: "https://train-schedule-8c9c6.firebaseio.com",
+	    projectId: "train-schedule-8c9c6",
+	    storageBucket: "train-schedule-8c9c6.appspot.com",
+	    messagingSenderId: "414203751368"
+  	};
+
+  	firebase.initializeApp(config);
+
+
+
+
+
+
+})
